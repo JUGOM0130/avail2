6cee6a347378e567ff57658cceb20abb1feb38d1
Junnosuke <junnehoomaki@yahoo.co.jp>
2022-09-28 13:35:17 +0900

    second commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        .mr10{
+            margin: 10px;
+        }
+        .fcon{
+            display: flex;
+            flex-direction: row;
+        }
+        .fitem{
+
+        }
+    </style>
+    <title>Index</title>
+</head>
+
+<body>
+    <div class="fcon">
+        <p class="mr10"><a href="progectlist.php">PROJECT</a></p>
+        <p class="mr10"><a href="#">KINTAI</a></p>
+        <p class="mr10"><a href="#">リンク3</a></p>
+        <p class="mr10"><a href="#">リンク4</a></p>
+
+    </div>
+</body>
+
+</html>
---
+++ progectlist.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <style>
+
+    </style>
+    <title>Index</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1>P_LIST</h1>
+        <div class="card mt-5" style="width: 18rem;">
+            <img src="./img/33612.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">ブレーキプレート Ambassadeur用</h5>
+                <p class="card-text">hogehoeg</p>
+                <a href="naiyo.php" class="btn btn-primary">こちら</a>
+            </div>
+        </div>
+        <div class="card mt-5" style="width: 18rem;">
+            <img src="./img/400_hd_sh_sti2_cgld.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">オフセットハンドル STi2.5 シマノ用 85mm </h5>
+                <p class="card-text">hugahuga</p>
+                <a href="#" class="btn btn-primary">こちら</a>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
