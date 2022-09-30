f430f86d69ca67f2a0ef04faf625ade4b7a58f85
Junnosuke <junnehoomaki@yahoo.co.jp>
2022-09-30 13:37:00 +0900

    アイコンを追加

--- index.php
+++ index.php
@@ -4,8 +4,14 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!--Bootstrap本体-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!--BootStrap icon-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
     <style>
         .mojisize_16 {
             font-size: 16pt;
@@ -16,7 +22,8 @@
 
 <body>
     <div class="container">
-        <h1 class="display-3 mt-3">メインメニュー</h1>
+    
+        <h1 class="display-3 mt-3"><i class="bi bi-house"></i> メインメニュー</h1>
         <ul>
             <li class="mb-2"><a class="mojisize_16" href="pdm_menu.php">PDMシステム</a></li>
             <li class="mb-2"><a class="mojisize_16" href="#">勤怠管理</a></li>
--- pdm_menu.php
+++ pdm_menu.php
@@ -5,7 +5,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <!--Bootstrap本体-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+        <!--BootStrap icon-->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
+
     <style>
         .mojisize_16 {
             font-size: 16pt;
@@ -22,15 +27,15 @@
                 <li class="breadcrumb-item active" aria-current="page">PDMメニュー</li>
             </ol>
         </nav>
-        <h1 class="display-3 mt-3">PDMメニュー</h1>
+        <h1 class="display-3 mt-3"><i class="bi bi-list"></i>PDMメニュー</h1>
         <ul>
-            <li class="mb-2"><a class="mojisize_16" href="progectlist.php">プロジェクトリスト</a></li>
-            <li class="mb-2"><a class="mojisize_16" href="#">CADデータ管理</a></li>
-            <li class="mb-2"><a class="mojisize_16" href="#">図面管理</a></li>
-            <li class="mb-2"><a class="mojisize_16" href="#">ドキュメント管理</a></li>
-            <li class="mb-2"><a class="mojisize_16" href="#">部品表（BOM）管理</a></li>
-            <li class="mb-2"><a class="mojisize_16" href="#">設計変更管理</a></li>
-            <li class="mb-2"><a class="mojisize_16" href="serch.php">検索</a></li>
+            <li class="mb-2"><a class="mojisize_16" href="progectlist.php"><i class="bi bi-list-check"></i> プロジェクトリスト</a></li>
+            <li class="mb-2"><a class="mojisize_16" href="#"><i class="bi bi-cloud-arrow-up"></i> CADデータ管理</a></li>
+            <li class="mb-2"><a class="mojisize_16" href="#"><i class="bi bi-cloud-arrow-up-fill"></i> 図面管理</a></li>
+            <li class="mb-2"><a class="mojisize_16" href="#"><i class="bi bi-files"></i> ドキュメント管理</a></li>
+            <li class="mb-2"><a class="mojisize_16" href="#"><i class="bi bi-gear-wide-connected"></i> 部品表（BOM）管理</a></li>
+            <li class="mb-2"><a class="mojisize_16" href="#"><i class="bi bi-hammer"></i> 設計変更管理</a></li>
+            <li class="mb-2"><a class="mojisize_16" href="serch.php"><i class="bi bi-search"></i> 検索</a></li>
         </ul>
 
 
